463dfac0077ff896a65bc2c260ac95b98ac933a3
Michele Imbriani <mbim@demant.com>
2024-02-22 16:51:04 +0100

    simpler definition (following docs)

--- subsys/bluetooth/audio/shell/bap_custom_tests.c
+++ subsys/bluetooth/audio/shell/bap_custom_tests.c
@@ -1,7 +1,8 @@
 #include <shell.h>
 
-static int cmd_bap_custom_tests_gatt_write_no_resp(void)
+static int cmd_gatt_write_no_resp(const struct shell *sh, size_t argc, char **argv)
 {
+	shell_print(sh, "HEHE");
 }
 
 static int cmd_bap_custom_tests(const struct shell *sh, size_t argc, char **argv)
@@ -15,15 +16,26 @@ static int cmd_bap_custom_tests(const struct shell *sh, size_t argc, char **argv
 	return -ENOEXEC;
 }
 
-SHELL_STATIC_SUBCMD_SET_CREATE(bap_tests_cmds,
-			       SHELL_CMD_ARG(gatt_write_no_response, NULL, "GATT Write no response",
-					     cmd_bap_custom_tests_gatt_write_no_resp, 0, 0),
-			       // ...
-);
+// static int cmd_default_handler(const struct shell *sh, size_t argc, char **argv)
+// {
+// 	if (argc == 1) {
+// 		shell_help(sh);
+// 		return SHELL_CMD_HELP_PRINTED;
+// 	}
 
-// SHELL_CMD_ARG_REGISTER(cmd_bap_custom_tests, &bap_custom_tests_cmds,
-// 		       "Bluetooth BAP broadcast assistant client shell commands",
-// 		       cmd_bap_broadcast_assistant, 1, 1);
+// 	shell_error(sh, "%s unknown parameter: %s", argv[0], argv[1]);
 
-SHELL_CMD_REGISTER(BAP_tests, &bap_tests_cmds, "Commands for missing BAP Tests",
-		   cmd_default_handler);
+// 	return -EINVAL;
+// }
+
+SHELL_STATIC_SUBCMD_SET_CREATE(
+	bap_tests_cmds,
+	// SHELL_CMD_ARG(gatt_write_no_response, NULL, "GATT Write no response",
+	//         cmd_bap_custom_tests_gatt_write_no_resp, 0, 0),
+	// ...
+	SHELL_CMD(gatt_write_no_resp, NULL, "GATT Write No Response", cmd_gatt_write_no_resp),
+	SHELL_SUBCMD_SET_END);
+
+SHELL_CMD_REGISTER(BAP_tests, &bap_tests_cmds,
+            "Commands for missing BAP Tests",
+            NULL);
